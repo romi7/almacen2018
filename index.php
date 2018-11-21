892b4b4fe1966b67faf14609215d52dc9db8b246
Administrator <Administrator@pc.localdomain>
2018-11-21 19:18:05 +0100

    el logic tiene casi terminada la interfaz definitiva me queda mejorar el registro y enlzar el login con la base de datos el error de si el usuario  o contraseña se introdujo mal y que la accion se correcta y pase a la siguiente pagina

--- index.php
+++ index.php
@@ -10,56 +10,76 @@ and open the template in the editor.
         <title></title>
          <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     </head>
-     <body style="background-image:url(imagenes/fondo_incio.png)">
+    <body style="background-color: #ffcccc">
          <div class ="container" id="primario" >
                 <br>  
                 <br> 
                      <br>  
                 <br> 
-                      <br>  
+                    
                 
-                 <div class="row" style="background-color:#ffffff"> 
-                     <div class="col-12" style="background-color:#ffffff" >  
-                     
-         <h2 class="text-center" style="color:black;"> <img src="imagenes/icono_sesion.png" class="img-fluid" alt="Responsive image" align="center"> </h2>      
-
-                     <br>
-                </div> 
-                     <div class="col-12" style="background-color:#ffffff" >  
-                         <h2 fo class="text-center" style="color:#007bff;"> INICIO DE SESION</h2>
-                </div> 
-                <div class="col-4">
-                
-                 </div>
-                <div class="col-4">
+                 <div class="row" style="background-color:#ffffff">  
+                      <div class="col-3"  style="background-color: #ffcccc"></div>
+                     <div class="col-6" > <h2 class="text-center" style="color:black;"> <img src="imagenes/icono_sesion.png" class="img-fluid" alt="Responsive image" align="center"> </h2> </div> 
+                       <div class="col-3"  style="background-color: #ffcccc"></div> 
+                       <br> 
+                       <br>
+                         <div class="col-3"  style="background-color: #ffcccc"></div>
+                     <div class="col-6" style="background-color:#ffffff" >  <h2 fo class="text-center" style="color:#007bff;"> INICIO DE SESION</h2></div> 
+                     <div class="col-3"  style="background-color: #ffcccc"></div> 
+                          <div class="col-3"  style="background-color: #ffcccc"></div>
+                <div class="col-6">
                     <br><br>
-                    
-                    <input id ="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required">
-                    <br>
-                    
-                    <input id ="cajaPassword" class="form-control" type="password" placeholder="Contraseña">
-                    <br>
-                  
-                    <button id="boton2" class="btn btn-primary btn-block">login</button>
-                    <br>
-                    
+                    <div class="row"> 
+                      <div class="col-3"></div>
+                        <div class="col-6" > <input  id ="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required"></div> 
+                              <div class="col-3"></div>
+                        <br>  <br>
+                      <div class="col-3"></div>
+                      <div class="col-6" ><input id ="cajaPassword" class="form-control" type="password" placeholder="Contraseña"> </div>
+                      <div class="col-3"></div>
+                    <br> 
+                     <br> 
+                         <br>
+                          <div class="col-3"></div>
+                          <div class="col-3" ><button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#btnAnyadir">Añadir</button></div>  
+                          
+                          <div class="col-3" ><button id="boton2" class="btn btn-primary btn-block">login</button></div>
+                          <div class="col-2"></div>
+                           <br>  <br>
+                    </div>
                     
        
                 </div>
-                <div class="col-4"></div>
+                <div class="col-3"  style="background-color: #ffcccc"></div>  
+                <br> 
+                <br>
+                  <div class="col-3"  style="background-color: #ffcccc"></div>
+                  <div class="col-6" style="background-color:#ffffff" > <h6  class="text-center" style="color:#007bff;">¿has olvidado tu contraseña?</h6></div> 
+                  <div class="col-3"  style="background-color: #ffcccc"></div>
+                
+                
                
             </div>
        
                 </div> 
     </body>
-     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
-    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script>
         //document ready se ejecuta cuando la página se ha cargado correctamente
        $(document).ready(function(){
            //$('#cajaNombre').hide(); 
-       });
+       }); 
+       
+           $('#botonRegristo').click(function(){
+           
+            
+            
+            $('#principal').load("regristo.php",{
+                
+            });
        
        
        
@@ -75,5 +95,34 @@ and open the template in the editor.
             });
         });
         
-    </script>
+    </script> 
+    
+    <div class="modal fade" id="btnAnyadir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" style="color:#007bff;" id="exampleModalLabel">Registro</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                  
+                    <input type="text" class="form-control"placeholder="Usuario" id="recipient-name">
+                    <br>
+                  
+                    <input type="text" class="form-control" placeholder="correo electronico" id="recipient-name">
+                    <br>
+                   
+                    <input type="text" class="form-control" placeholder="password" id="recipient-name"> 
+                    <br>
+                    <input type="text" class="form-control" placeholder="codigo de empleado" id="recipient-name">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Añadir</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </html>
